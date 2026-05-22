2f080d65a90c655c5ffb31d9fd22c5113112b36a
DeviBhoi <sunitabhoi612@gmail.com>
2026-05-22 10:05:32 +0530

    Deployment ready

--- app.py
+++ app.py
@@ -1045,3 +1045,7 @@ def logout():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+    @app.route("/")
+    def home():
+        return "Chatbot AI Project Running Successfully!"
